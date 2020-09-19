e58864d605be63f347611d72034dd7823bb6c9d7
Francis Rodrigues <francis.rod01@outlook.com>
2020-09-18 21:06:48 -0300

    wip: Generated admin manage heroes component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,15 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './admin/admin.component';
 import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
+import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
 
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
     AdminComponent,
-    ManageCrisesComponent
+    ManageCrisesComponent,
+    ManageHeroesComponent
   ],
   imports: [
     CommonModule,
