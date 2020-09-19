a672686c3d68d0c0fba4bd46bbe415b857867ca3
Francis Rodrigues <francis.rod01@outlook.com>
2020-09-18 21:05:16 -0300

    wip: Generated admin manage crises component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './admin/admin.component';
+import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
 
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
-    AdminComponent
+    AdminComponent,
+    ManageCrisesComponent
   ],
   imports: [
     CommonModule,
