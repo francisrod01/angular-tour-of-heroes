ddbce9ec19ce894672d8fa7646db14e2f6e33fc1
Francis Rodrigues <francis.rod01@outlook.com>
2020-09-20 15:56:31 -0300

    wip: Imported the admin routing and its module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
 import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
 
+import { AdminRoutingModule } from './admin-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AdminDashboardComponent,
-    AdminComponent,
-    ManageCrisesComponent,
-    ManageHeroesComponent
-  ],
   imports: [
     CommonModule,
     AdminRoutingModule
+  ],
+  declarations: [
+    AdminComponent,
+    AdminDashboardComponent,
+    ManageCrisesComponent,
+    ManageHeroesComponent
   ]
 })
 export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -15,30 +16,34 @@ import { CrisisCenterModule } from './crisis-center/crisis-center.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroSearchComponent,
-    MessagesComponent,
-    PageNotFoundComponent,
-    ComposeMessageComponent
-  ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
     CrisisCenterModule,
+    AdminModule,
     AppRoutingModule,
     HttpClientModule,
     // It intercepts HTTP requests and returns simulated server responses
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     ),
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    MessagesComponent,
+    ComposeMessageComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
