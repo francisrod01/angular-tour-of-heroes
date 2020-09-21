7c6d6be3931cbc3528dc11c414ebfe3daa720e79
Francis Rodrigues <francis.rod01@outlook.com>
2020-09-21 00:33:29 -0300

    wip: Guard the admin feature
    Set the admin routing with a canActivate guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../auth/auth.guard';
+
 import { AdminComponent } from './admin/admin.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
@@ -10,6 +12,7 @@ const adminRoutes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
